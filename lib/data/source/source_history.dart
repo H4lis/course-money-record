22b07a598d9c983003088ce438232361451ddf57
H4lis <nurhalis092@gmail.com>
2024-01-15 19:07:57 +0800

    add source history

--- lib/data/source/source_history.dart
+++ lib/data/source/source_history.dart
@@ -1,4 +1,18 @@
+import 'package:intl/intl.dart';
+
+import '../../config/api.dart';
+import '../../config/app_request.dart';
+import '../../config/session.dart';
+import '../model/user.dart';
+
 class SourceHistory {
-  
-  
+  static Future<Map> login(String idUser, String password) async {
+    String url = '${Api.user}/analysis.php';
+    Map? responseBody = await AppRequest.post(url, {
+      'id_user': idUser,
+      'today': DateFormat('yyyy-MM-dd').format(DateTime.now()),
+    });
+    if (responseBody == null) return {};
+    return responseBody['success'];
+  }
 }
