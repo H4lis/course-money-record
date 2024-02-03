17a763adae5698973c8b67716c4621733f95f156
NUR HALIS <nurhalis092@gmail.com>
2024-02-03 11:02:30 +0800

    Add history Page UI2

--- lib/presentation/page/history/add_hisory_page.dart
+++ lib/presentation/page/history/add_hisory_page.dart
@@ -1,6 +1,8 @@
 import 'package:d_input/d_input.dart';
 import 'package:d_view/d_view.dart';
 import 'package:flutter/material.dart';
+import 'package:money_record/config/app.format.dart';
+import 'package:money_record/config/app_color.dart';
 
 class AddHistoryPage extends StatelessWidget {
   const AddHistoryPage({super.key});
@@ -54,6 +56,72 @@ class AddHistoryPage extends StatelessWidget {
             title: 'Harga',
             inputType: TextInputType.number,
           ),
+          DView.spaceHeight(),
+          Center(
+            child: Container(
+              height: 4,
+              width: 80,
+              decoration: BoxDecoration(
+                color: AppColor.bg,
+                borderRadius: BorderRadius.circular(30),
+              ),
+            ),
+          ),
+          DView.spaceHeight(),
+          Text("Items", style: const TextStyle(fontWeight: FontWeight.bold)),
+          DView.spaceHeight(4),
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: BoxDecoration(
+                border: Border.all(
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(4)),
+            child: Wrap(
+              children: [
+                Chip(
+                  label: Text(
+                    'Sumber',
+                  ),
+                  deleteIcon: Icon(Icons.clear),
+                  onDeleted: () {},
+                )
+              ],
+            ),
+          ),
+          DView.spaceHeight(),
+          Row(
+            children: [
+              Text("Total:",
+                  style: const TextStyle(fontWeight: FontWeight.bold)),
+              DView.spaceWidth(8),
+              Text(
+                Appformat.currency('300000'),
+                style: Theme.of(context).textTheme.headline5!.copyWith(
+                    fontWeight: FontWeight.bold, color: AppColor.primary),
+              ),
+            ],
+          ),
+          DView.spaceHeight(30),
+          Material(
+            color: AppColor.primary,
+            borderRadius: BorderRadius.circular(8),
+            child: InkWell(
+              onTap: () {},
+              borderRadius: BorderRadius.circular(8),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Center(
+                    child: Text(
+                  'SUBMIT',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headline6!
+                      .copyWith(color: Colors.white),
+                )),
+              ),
+            ),
+          )
         ],
       ),
     );
